846473ed5a5ac77d1e64ee94eba5478d80c9219a
Charlie <charliekapsiak@gmail.com>
2020-06-10 09:41:42 -0500

    cuttout requester now integrates better with database and added some threading

--- solar/solar_retrieval/cutout_requester.py
+++ solar/solar_retrieval/cutout_requester.py
@@ -11,6 +11,15 @@ from solar.common.solar_event import Solar_Event
 import solar.solar_database.database as db
 
 
+def get_ssw_from_database(connection,event_id):
+    data = connection.execute("SELECT (ssw_job_id) FROM hek_events WHERE event_id=? AND NOT ssw_job_id='NO_REQUEST'",(event_id,))
+    ret = data.fetchone()
+    if ret:
+        return ret[0]
+    else:
+        return False
+
+
 def download_file(from_url, target):
     with open(target, "wb") as f:
         f.write(requests.get(from_url).content)
@@ -67,6 +76,16 @@ class Cutout_Request:
         #currently unused
         self.verbose = verbose
 
+        self.connection = db.get_connection()
+
+
+        self.ssw_in_database = False
+
+
+
+    def add_to_database(self,connection):
+        if self.job_id:
+            db.update_record(connection, self.e, 'ssw_job_id' , self.job_id)
 
     def request(self):
         """
@@ -76,47 +95,52 @@ class Cutout_Request:
             self.job_id -> The job id of the ssw_process
          
         """
-        try:
-            self.response = requests.get(
-                self.base_url,
-                params={
-                    "starttime": self.e.start_time,
-                    "endtime": self.e.end_time,
-                    "instrume": self.e.instrument,
-                    "notrack": self.notrack,
-                    "xcen": self.xcen,
-                    "ycen": self.ycen,
-                    "fovx": self.fovx,
-                    "fovy": self.fovy,
-                    "max_frames": 10,
-                    "waves": self.waves,
-                    "queue_job": 1,
-                },
-            )
-        except HTTPError as http_err:
-            print(f"HTTP error occurred: {http_err}")  # Python 3.6
-        except Exception as err:
-            print(f"Other error occurred: {err}")  # Python 3.6
-        else:
-            print(
-                f"Successfully submitted request "
-                f"from times {self.e.start_time}"
-                f" to {self.e.end_time} "
-            )
-            self.data = self.response.text
-            self.job_id = re.search('<param name="JobID">(.*)</param>', self.data)[1]
-            self.data_response_url = self.data_response_url_template.format(
-                ssw_id=self.job_id
-            )
-            self.add_to_database(db.get_connection())
-            
-
         
-    def add_to_database(self, connection):
+        self.job_id = get_ssw_from_database(db.get_connection(),self.e.event_id)
+        print(self.job_id)
         if self.job_id:
-            db.update_record(connection, self.e, 'ssw_job_id' , self.job_id)
-        
+            temp_url = self.data_response_url_template.format(
+            ssw_id=self.job_id
+        )
 
+            print(f"""Looks like the event with id {self.e.event_id} is already in process 
+                    The ID is: {self.job_id}
+                    The status may be views at {temp_url}
+                    """)
+        else:
+            try:
+                self.response = requests.get(
+                    self.base_url,
+                    params={
+                        "starttime": self.e.start_time,
+                        "endtime": self.e.end_time,
+                        "instrume": self.e.instrument,
+                        "notrack": self.notrack,
+                        "xcen": self.xcen,
+                        "ycen": self.ycen,
+                        "fovx": self.fovx,
+                        "fovy": self.fovy,
+                        "max_frames": 10,
+                        "waves": self.waves,
+                        "queue_job": 1,
+                    },
+                )
+            except HTTPError as http_err:
+                print(f"HTTP error occurred: {http_err}")  # Python 3.6
+            except Exception as err:
+                print(f"Other error occurred: {err}")  # Python 3.6
+            else:
+                print(
+                    f"Successfully submitted request "
+                    f"from times {self.e.start_time}"
+                    f" to {self.e.end_time} "
+                )
+                self.data = self.response.text
+                self.job_id = re.search('<param name="JobID">(.*)</param>', self.data)[1]
+                self.add_to_database(db.get_connection())
+        self.data_response_url = self.data_response_url_template.format(
+            ssw_id=self.job_id
+        )
 
     def fetch_data(self):
         if self.job_id == None:
@@ -124,13 +148,13 @@ class Cutout_Request:
 
         data_acquired = False
         while not data_acquired:
-            time.sleep(self.delay_time)
-            print(f"Attempting to fetch data from {self.data_response_url}") 
             self.data_response = requests.get(self.data_response_url)
             if re.search("Per-Wave file lists", self.data_response.text):
                 data_acquired = True
             else:
                 print("Data not available") 
+            time.sleep(self.delay_time)
+            print(f"Attempting to fetch data from {self.data_response_url}") 
         print("Data now available") 
         if data_acquired:
             fits_list_url = re.search(
@@ -146,19 +170,29 @@ class Cutout_Request:
     def download_fits_files(self, save_dir_loc=None):
         save_dir = save_dir_loc if save_dir_loc else self.save_dir
         self.save_dir.mkdir(parents=True, exist_ok=True)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
-            for fits_file_url in self.file_list:
+        download_list = [file_path for file_path in self.file_list if not (save_dir / fits_file_url).exists()]
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            for fits_file_url in download_list:
                 executor.submit(download_file, self.data_response_url + fits_file_url , save_dir / fits_file_url)
 
+    def execute_full(self,connection):
+        self.job_id = self.get_ssw_from_database(connection)
+        if not self.job_id:
+            self.request()
+        self.fetch_data()
+        self.download_fits_files()
 
 
 
+def cuttout_wrapper(event):
+    c = Cutout_Request(c)
+    c.execute_full()
 
 
-if __name__ == "__main__":
-    s = Cutout_Request(
-        "2011-11-30T09:00:02", "2011-11-30T09:40:04", -199, 507, 1341, 350
-    )
-    s.request()
-    s.fetch_data()
-    s.download_fits_files()
+def multithread_cuttout(list_of_events):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=500) as executor:
+        for event in list_of_events:
+            executor.submit(cuttout_wrapper,event)
+
+
+
