dad641c4d51e20b227ded765e99f92c1091fc2a0
Charlie <charliekapsiak@gmail.com>
2020-06-14 13:53:50 -0500

    No longer need utils

--- solar/database/utils.py
+++
@@ -1,8 +0,0 @@
-import peewee as pw
-from solar.database import database
-from solar.database.tables import TABLES, Solar_Event, Fits_File
-from pathlib import Path
-
-from datetime import datetime
-
-import re
