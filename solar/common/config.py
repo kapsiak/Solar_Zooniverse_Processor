3b06fc9fa2e74e997d24efb39400a26b69ac1bca
Charlie <charliekapsiak@gmail.com>
2020-06-18 22:22:58 -0500

    New map class in the config to allow for easier access to member variables

--- solar/common/config.py
+++ solar/common/config.py
@@ -3,15 +3,47 @@ class ConfMeta(type):
         return cls.__dict__[key]
 
 
+class Map(dict):
+    """
+    Example:
+    m = Map({'first_name': 'Eduardo'}, last_name='Pool', age=24, sports=['Soccer'])
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Map, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for k, v in arg.items():
+                    self[k] = v
+
+        if kwargs:
+            for k, v in kwargs.items():
+                self[k] = v
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Map, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Map, self).__delitem__(key)
+        del self.__dict__[key]
+
+
 class Config(metaclass=ConfMeta):
     db_path = "test.db"
     db_save = "files"
-    storage_path = dict(
-            fits = "fits/{event_id}/{server_file_name}"
-        , img = "generated/{image_type}/{sol_standard}/{file_name}"
-            )
-    time_format = dict(
-    hek = "%Y-%m-%dT%H:%M:%S"
-    fits = "%Y-%m-%dT%H:%M:%S.%f"
+    storage_path = Map(
+        fits="fits/{event_id}/{server_file_name}",
+        img="generated/{image_type}/{sol_standard}/{file_name}",
     )
+    time_format = Map(hek="%Y-%m-%dT%H:%M:%S", fits="%Y-%m-%dT%H:%M:%S.%f")
     chatty = True
