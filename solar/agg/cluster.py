5f2ce82e40f54cc32951eb9e483756af80143ae9
Charlie <charliekapsiak@gmail.com>
2020-07-27 09:24:30 -0500

    Structures ans some initial clustering algorithms

---
+++ solar/agg/cluster.py
@@ -0,0 +1,26 @@
+from sklearn.cluster import AffinityPropagation
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn import metrics
+from sklearn.datasets import make_blobs
+
+
+def aff_fit(data):
+    """TODO: Docstring for fit_points.
+
+    :param point_list: TODO
+    :returns: TODO
+
+    """
+    af = AffinityPropagation(preference=-50).fit(data)
+    cluster_centers_indices = af.cluster_centers_indices_
+    return cluster_centers_indices
+
+
+def mean_fit(data):
+    bandwidth = estimate_bandwidth(data, quantile=0.2, n_samples=500)
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(data) 
+    labels = ms.labels_
+    cluster_centers = ms.cluster_centers_
+    return (labels, cluster_centers)
+
---
+++ solar/agg/structs.py
@@ -0,0 +1,92 @@
+from dataclasses import dataclass
+from solar.database.tables.fits_file import Fits_File
+from solar.database.tables.visual_file import Visual_File
+from datetime import datetime, timedelta
+from solar.zooniverse.structs import ZPoint, ZRect
+
+
+TIME_START = datetime(2000, 1, 1, 0, 0, 0)
+
+
+def since_start(time):
+    return (time - TIME_START) / timedelta(milliseconds=1)
+
+
+@dataclass
+class Space_Obj:
+    subject_id: int = 0
+    user_id: int = 0
+    workflow_id: int = 0
+    class_id: int = 0
+
+    fits_id: int = -1
+    visual_id: int = -1
+    frame: int = -1
+
+    time: datetime = datetime(1990, 1, 1)
+
+    hpc_x: float = -1
+    hpc_y: float = -1
+
+    purpose: str = None
+
+    def make_data(self):
+        raise NotImplementedError
+
+    def __set_time(self, z_struct):
+        f = Fits_File.get(Fits_File.id == self.fits_id)
+        self.time = f.image_time
+
+    def __set_hpcxy(self, z_struct):
+        v = Visual_File.get(Visual_File.id == self.visual_id)
+        coord = v.world_from_pixel(z_struct.x, z_struct.y)
+        self.hpc_y, self.hpc_x = coord.spherical.lat.arcsec, coord.spherical.lon.arcsec
+
+    @classmethod
+    def base_make(cls, z_struct):
+        new = cls(
+            subject_id=z_struct.subject_id,
+            user_id=z_struct.user_id,
+            workflow_id=z_struct.workflow_id,
+            class_id=z_struct.class_id,
+            fits_id=z_struct.fits_id,
+            visual_id=z_struct.visual_id,
+            frame=z_struct.frame,
+        )
+        new.__set_hpcxy(z_struct)
+        new.__set_time(z_struct)
+        return new
+
+    @staticmethod
+    def make(z_struct):
+        if isinstance(z_struct, ZPoint):
+            return Space_Point.make(z_struct)
+        if isinstance(z_struct, ZRect):
+            return Space_Rect.make(z_struct)
+        return None
+
+
+@dataclass
+class Space_Point(Space_Obj):
+    def make_data(self):
+        return (self.hpc_x, self.hpc_y, since_start(self.time))
+
+    @classmethod
+    def make(cls, z_struct):
+        return super().base_make(z_struct)
+
+
+@dataclass
+class Space_Rect(Space_Obj):
+    w: float = -1
+    h: float = -1
+    a: float = -1
+
+    def make_data(self):
+        return (self.hpc_x, self.hpc_y, self.w, self.h, self.a, since_start(self.time))
+
+    @classmethod
+    def make(cls, z_struct):
+        new = super().base_make(z_struct)
+        new.w, new.h, new.a = z_struct.w, z_struct.h, z_struct.a
+        return new
